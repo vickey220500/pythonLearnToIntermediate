03cae495ba69656539bf0c5eaf87eb6bbd77ea89
vickey220500 <vickey220500@gmail.com>
2025-03-19 15:11:08 +0530

    changes

--- importantLibrary.py
+++ importantLibrary.py
@@ -9,3 +9,6 @@ cv.imshow('show',image)
 cv.imshow('showresize',resizeImg)
 cv.imshow('gaussianImag',gaussianImag)
 cv.imshow('thresholdImag',thresholdImg)
+# cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) Thickess code
+# cv.putText(img,text,(10,20),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2) Text generator 
+# cv.findContours(threshImg.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE) its for get the border of the object
